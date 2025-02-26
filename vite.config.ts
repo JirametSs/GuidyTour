a8ee72049a74a867875cef92f5ce65196fcf009c
Jiramet Phumwatchara <164776696+JirametSs@users.noreply.github.com>
2025-02-26 16:58:14 +0700

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/GuidyTour/', // 👈 ต้องตรงกับชื่อ repo ใน GitHub
+  base: '/GuidyTour/',
 });
